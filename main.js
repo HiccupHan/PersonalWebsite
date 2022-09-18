ef5e46054a44ac4cdc6b21aa86d32ee2a108ed29
HiccupHan <89772977+HiccupHan@users.noreply.github.com>
2022-09-17 18:38:26 -0700

    updated main page
    updated splash screen with a greeting

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+const header = document.querySelector('header');
+let prevScrollPos = window.pageYOffset;
 
+function hideNav() {
+    document.getElementById("nav-toggle").checked = false;
+    header.classList.add('hide');
+}
+
+function showNav() {
+    header.classList.remove('hide');
+}
+
+window.addEventListener('scroll', () =>{
+    let curScrollPos = window.pageYOffset;
+
+    if(prevScrollPos < curScrollPos){
+        hideNav();
+    }
+    else{
+        showNav();
+    }
+    prevScrollPos=curScrollPos;
+})
