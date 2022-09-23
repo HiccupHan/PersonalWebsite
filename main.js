d39ed2b6706bf08b6108a036b7dc77b28c80646b
HiccupHan <89772977+HiccupHan@users.noreply.github.com>
2022-09-22 19:24:00 -0700

    changed nav bar and added flip cards
    Added flip cards with personal information
    Still need to center text and work out small screen implementation
    Found an issue where the width of the website is bigger than the browser's available space

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
 const header = document.querySelector('header');
 let prevScrollPos = window.pageYOffset;
 
-function hideNav() {
-    document.getElementById("nav-toggle").checked = false;
-    header.classList.add('hide');
-}
-
-function showNav() {
-    header.classList.remove('hide');
-}
-
 window.addEventListener('scroll', () =>{
     let curScrollPos = window.pageYOffset;
 
     if(prevScrollPos < curScrollPos){
-        hideNav();
+        document.getElementById('nav__toggle').checked = false;
+        header.style.top = '-73px';
     }
     else{
-        showNav();
+        header.style.top = '0';
     }
     prevScrollPos=curScrollPos;
 })
